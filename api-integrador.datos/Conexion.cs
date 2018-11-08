55954fa6ad418b091e4c92aa355a4e22ae05202b
unknown <mfab456@outlook.com>
2018-11-08 10:52:42 -0500

    commit

---
+++ api-integrador.datos/Conexion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api_integrador.datos
+{
+    public class Conexion
+    {
+        SqlConnection cn;
+        public SqlConnection conecta()
+        {
+            cn = new SqlConnection(
+            "server=.;database=BDINTEGRADOR;Integrated Security=true");
+            return cn;
+        }
+    }
+}
---
+++ api-integrador.modelos/Tutor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api_integrador.modelos
+{
+    public class Tutor
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string direccion { get; set; }
+        public string apellido { get; set; }
+        public string email { get; set; }
+        public string contraseña { get; set; }
+    }
+}
