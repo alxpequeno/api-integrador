690c0e5e30d1cde9c1a9104232946a72cecc6501
unknown <mfab456@outlook.com>
2018-11-08 16:15:21 -0500

    commit 5

--- api-integrador/Controllers/TutorController.cs
+++ api-integrador/Controllers/TutorController.cs
@@ -42,15 +42,15 @@ namespace api_integrador.Controllers
         public string ActualizarTutor(Tutor tutor)
         {
             string mensaje = "";
-            mensaje = negociosT.ActualizarTrabajador(trabajador);
+            mensaje = negociosT.ActualizarTutor(tutor);
             return mensaje;
         }
 
-        [HttpGet]
-        public string EliminarTutor(int idTutor)
+        [HttpPost]
+        public string EliminarTutor(Tutor tutor)
         {
             string mensaje = "";
-            mensaje = negociosT.EliminarTutor(idTutor);
+            mensaje = negociosT.EliminarTutor(tutor.idTutor);
             return mensaje;
         }
 
