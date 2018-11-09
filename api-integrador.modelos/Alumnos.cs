19c8c1877ef9abb5b2b11278008c875040b6f18f
JesusAndresLT <jesusandreslt@hotmail.com>
2018-11-09 16:56:50 -0500

    commit 6

---
+++ api-integrador.modelos/Alumnos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api_integrador.modelos
+{
+    public class Alumnos
+    {
+        public int idAlumno { get; set; }
+        public string emailAlumno { get; set; }
+        public string nombreAlumno { get; set; }
+        public string apellidoAlumno { get; set; }
+        public string contraseñaAlumno { get; set; }
+
+
+
+    }
+}
