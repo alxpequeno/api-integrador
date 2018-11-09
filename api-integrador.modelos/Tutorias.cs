2318cb4291b22725742f4319306da25bf54f862c
JesusAndresLT <jesusandreslt@hotmail.com>
2018-11-09 17:01:04 -0500

    commit 6

---
+++ api-integrador.modelos/Tutorias.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api_integrador.modelos
+{
+    public class Tutorias
+    {
+        public int idTutoria { get; set; }
+        public string codigoTutoria { get; set; }
+        public string tituloTutoria { get; set; }
+        public string categoriaTutoria { get; set; }
+        public string fechaTutoria { get; set; }
+        public string horaTutoria { get; set; }
+        public string ubicacionTutoria { get; set; }
+        public double precioTutoria { get; set; }
+        public string descipcionTutoria { get; set; }
+        public string idA { get; set; }
+        public string id_tutor { get; set; }
+
+    }
+}
