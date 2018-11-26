e7a56628c422578e27ac6dd1e5e9b758e2ed0e39
Alexis Pequeño <alxpequeno@gmail.com>
2018-11-25 23:07:26 -0500

    correcion proc e inserts

--- BDINTEGRADOR-2.sql
+++ BDINTEGRADOR-2.sql
@@ -16,6 +16,46 @@ IF OBJECT_ID('SP_OBTENERTUTORXID') IS NOT NULL
     DROP PROCEDURE  SP_OBTENERTUTORXID
 GO
 
+IF OBJECT_ID('SP_TUTOR_DELETE') IS NOT NULL
+    DROP PROCEDURE  SP_TUTOR_DELETE
+GO
+
+IF OBJECT_ID('SP_TUTOR_LISTAR') IS NOT NULL
+    DROP PROCEDURE  SP_TUTOR_LISTAR
+GO
+
+IF OBJECT_ID('SP_TUTOR_UPDATE') IS NOT NULL
+    DROP PROCEDURE  SP_TUTOR_UPDATE
+GO
+
+IF OBJECT_ID('SP_EMP_DELETE') IS NOT NULL
+    DROP PROCEDURE  SP_EMP_DELETE
+GO
+
+IF OBJECT_ID('SP_EMP_UPDATE') IS NOT NULL
+    DROP PROCEDURE  SP_EMP_UPDATE
+GO
+
+IF OBJECT_ID('SP_EMP_INSERT') IS NOT NULL
+    DROP PROCEDURE  SP_EMP_INSERT
+GO
+
+IF OBJECT_ID('SP_EMP_LISTAR') IS NOT NULL
+    DROP PROCEDURE  SP_EMP_LISTAR
+GO
+
+IF OBJECT_ID('SP_ALUMNO_LISTAR') IS NOT NULL
+    DROP PROCEDURE  SP_ALUMNO_LISTAR
+GO
+
+IF OBJECT_ID('SP_ALUMNO_INSERT') IS NOT NULL
+    DROP PROCEDURE  SP_ALUMNO_INSERT
+GO
+
+IF OBJECT_ID('SP_OBTENER_ALUMNO_X_ID') IS NOT NULL
+    DROP PROCEDURE  SP_OBTENER_ALUMNO_X_ID
+GO
+
 /* DROP TABLES */
 
 IF OBJECT_ID('USUARIO') IS NOT NULL
@@ -169,19 +209,78 @@ go
 
 
 /* INSERTS */
-EXEC SP_TUTOR_INSERT 'Julio','Profe','Av. youtube','jp@juliprofe.com','123'
 
-INSERT USUARIO(Nombre,Apellido,Direccion,Email,Clave,isEmpleado,isTutor,isAlumno,Estado) 
-    VALUES ('a', 'b', 'c', 'admin', 'admin', 'true', 'false', 'false','true')
+/*IsEmpleado*/
+INSERT INTO USUARIO(Nombre,Apellido,Direccion,Email,Clave,isEmpleado,isTutor,isAlumno,Estado) 
+VALUES('Angel','Leon','Av. El Sol', 'angel.leon@gmail.com','12345','true','false','false','true')
+GO
 
-/* SELECTS */
+INSERT INTO USUARIO(Nombre,Apellido,Direccion,Email,Clave,isEmpleado,isTutor,isAlumno,Estado) 
+VALUES('Mayra','Lescano','Calle los Olivos', 'solange_1423@gmail.com','9876543','true','false','false','true')
+GO
 
-exec SP_OBTENERTUTORXID 
-select * from usuarios
-EXEC SP_LISTA_TUTORES_PENDIENTES
+INSERT INTO USUARIO(Nombre,Apellido,Direccion,Email,Clave,isEmpleado,isTutor,isAlumno,Estado) 
+VALUES('Hugo','Quisoe','La Estancia', 'h.quispe@gmail.com','0987876','true','false','false','true')
+GO
 
+INSERT INTO USUARIO(Nombre,Apellido,Direccion,Email,Clave,isEmpleado,isTutor,isAlumno,Estado) 
+VALUES('Marcello','Sanchez','La Molina', 'mcllo.s@gmail.com','algoritmo1234','true','false','false','true')
+GO
 
-select*from USUARIO
+INSERT INTO USUARIO(Nombre,Apellido,Direccion,Email,Clave,isEmpleado,isTutor,isAlumno,Estado) 
+VALUES('Luis','Acosta','Callao', 'acosta.luis@gmail.com','casita123','true','false','false','true')
+GO
+
+/*IsAlumno*/
+INSERT INTO USUARIO(Nombre,Apellido,Direccion,Email,Clave,isEmpleado,isTutor,isAlumno,Estado) 
+VALUES('Yesenia','Tesen','Punta Negra', 'yes.tesen@gmail.com','123456','false','false','true','true')
+GO
+
+INSERT INTO USUARIO(Nombre,Apellido,Direccion,Email,Clave,isEmpleado,isTutor,isAlumno,Estado) 
+VALUES('Jhoel','Campos','Miraflores', 'jcampost@gmail.com','567890','false','false','true','true')
+GO
+
+INSERT INTO USUARIO(Nombre,Apellido,Direccion,Email,Clave,isEmpleado,isTutor,isAlumno,Estado) 
+VALUES('Liliana','Benites','La Victoria', 'lili.benites@gmail.com','sillita453','false','false','true','true')
+GO
+
+INSERT INTO USUARIO(Nombre,Apellido,Direccion,Email,Clave,isEmpleado,isTutor,isAlumno,Estado) 
+VALUES('Fernandina','Allca','Lurin', 'fer.nandina@gmail.com','fer1234','false','false','true','true')
+GO
+
+INSERT INTO USUARIO(Nombre,Apellido,Direccion,Email,Clave,isEmpleado,isTutor,isAlumno,Estado) 
+VALUES('David','Leon','Lurin', 'elrey.leon@gmail.com','davidleon','false','false','true','true')
+GO
+
+/*IsEmpleado*/
+INSERT INTO USUARIO(Nombre,Apellido,Direccion,Email,Clave,isEmpleado,isTutor,isAlumno,Estado,FechaRegistro)  
+VALUES('Alonso','Vasquez','San Isidro', 'vasquez.alonso@gmail.com','alonso1234','false','true','false','true','12/12/2017')
+GO
+INSERT INTO USUARIO(Nombre,Apellido,Direccion,Email,Clave,isEmpleado,isTutor,isAlumno,Estado,FechaRegistro)  
+VALUES('Daniel','Gonzales','El Romac', 'dani.g@gmail.com','daniel12344','false','true','false','true','03/04/2018')
+GO
+INSERT INTO USUARIO(Nombre,Apellido,Direccion,Email,Clave,isEmpleado,isTutor,isAlumno,Estado,FechaRegistro)  
+VALUES('Thommy','Susanivar','San Miguel', 'susanivar@gmail.com','thommy1234','false','true','false','true','03/06/2018')
+GO
+INSERT INTO USUARIO(Nombre,Apellido,Direccion,Email,Clave,isEmpleado,isTutor,isAlumno,Estado,FechaRegistro)  
+VALUES('Mariella','Meza','San Miguel', 'mmeza@gmail.com','mariella1234','false','true','false','true','05/02/2018')
+GO
+INSERT INTO USUARIO(Nombre,Apellido,Direccion,Email,Clave,isEmpleado,isTutor,isAlumno,Estado,FechaRegistro)  
+VALUES('Miguel','Miranda','Los Olvivos', 'miguel.miranda@gmail.com','miki1234','false','true','false','true','10/10/2018')
+GO
+
+/*INSERT DETALLE TUTOR*/
+
+INSERT INTO DETALLE_TUTOR VALUES(11,'true','https://www.psicoactiva.com/blog/wp-content/uploads/2017/07/hombre-soltero-feliz.jpg',null,null,null)
+GO
+INSERT INTO DETALLE_TUTOR VALUES(12,'true','https://afemefa.com/wp-content/uploads/2017/02/mujer-obesa.jpg',null,null,null)
+GO
+INSERT INTO DETALLE_TUTOR VALUES(13,'true','http://www.laredcoaching.com/wp-content/uploads/2016/07/Hombre-pensando-mientras-se-toca-la-barbilla-con-la-mano.jpg',null,null,null)
+GO
+INSERT INTO DETALLE_TUTOR VALUES(14,'true','http://i.shangc.net/2017/0320/20170320094441281.jpg',null,null,null)
+GO
+INSERT INTO DETALLE_TUTOR VALUES(15,'true','https://www.abc.es/media/recreo/2016/09/12/Fotolia_32201374_Subscription_XXL--478x270-kmyG--620x349@abc.jpg',null,null,null)
+GO
 
 
 /*Procedure Alumno*/
