6b46dba95bbbffd9aef85e0e01cadfd6d4c6a697
Alexis Pequeño <alxpequeno@gmail.com>
2018-12-06 21:13:13 -0500

    pago agregado

--- api-integrador/Controllers/PagoController.cs
+++ api-integrador/Controllers/PagoController.cs
@@ -6,14 +6,16 @@ using System.Net.Http;
 using System.Web.Http;
 using api_integrador.Models;
 using api_integrador.negocios;
+using System.Web.Http.Cors;
 
 namespace api_integrador.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class PagoController : ApiController
     {
 
         ValidarPagoNegocios negocios = new ValidarPagoNegocios();
-        [HttpPost]
+        [HttpPost, HttpOptions]
         public ValidarPagoResponse ValidarPago(ValidarPagoRequest request)
         {
             ValidarPagoResponse response = new ValidarPagoResponse();
